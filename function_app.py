fa1e2b98db254276578dec588c8d6c7a22d8e9ca
dennisvandijk-Git <dennisvandijk91@live.nl>
2024-10-02 22:43:37 +0200

    Added proxy function for HTTP_calls, keeps func.key save

--- function_app.py
+++ function_app.py
@@ -1,13 +1,40 @@
 import azure.functions as func
 import os
 import json
+import requests
 import datetime
-import logging
 from azure.data.tables import TableClient
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp()
 
-@app.route(route="http_trigger_visitor_counter")
+@app.function_name('HttpAccess')
+@app.route(route="access-key", auth_level=func.AuthLevel.ANONYMOUS)
+
+def access_key(req: func.HttpRequest) -> func.HttpResponse:
+
+    real_function_url = "https://visitor-counter-api-http-trigger.azurewebsites.net/api/http_trigger_visitor_counter"
+
+    api_key = os.getenv("API_KEY")
+
+    headers = {
+        'x-functions-key': api_key,
+        'Content-Type': 'application/json'
+    }
+
+    real_function_response = requests.get(real_function_url, headers=headers)
+    print(type(real_function_response))
+    # print(json.dumps(real_function_response))
+    responze = [{"visitsCount": real_function_response.json()}]
+    # print(responze)
+    # print(json.dumps(responze))
+
+    return func.HttpResponse(
+        json.dumps(responze),
+        status_code=real_function_response.status_code,
+        mimetype="application/json"
+    )
+
+@app.route(route="http_trigger_visitor_counter", auth_level=func.AuthLevel.FUNCTION)
 
 def http_trigger_visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
     connection_string = os.getenv("CosmosDbConnectionSetting")
@@ -49,14 +76,19 @@ def http_trigger_visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
             'RowKey': visitor_ip,
             'VisitorCount': max_visitor_count
         })
+        # response_kvp = [{"visitCount": max_visitor_count}]
+        # response_json = json.dumps(response_kvp)
         response_json = json.dumps(max_visitor_count)
         return func.HttpResponse(response_json, mimetype="application/json", status_code=200)
     else:
         # do this - increment count + 1
+        increment_visitor_count = max_visitor_count + 1
         table_client.create_entity(entity={
             'PartitionKey': date,
             'RowKey': visitor_ip,
-            'VisitorCount': max_visitor_count +1
+            'VisitorCount': increment_visitor_count
         })
-        response_json = json.dumps(max_visitor_count + 1)
+        # response_kvp = [{"visitCount": increment_visitor_count}]
+        # response_json = json.dumps(response_kvp)
+        response_json = json.dumps(max_visitor_count)
         return func.HttpResponse(response_json, mimetype="application/json", status_code=200)
