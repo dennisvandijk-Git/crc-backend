38a40852fe719f2b5b21aed6450f0782ab545ee8
dennisvandijk-Git <dennisvandijk91@live.nl>
2024-10-05 21:39:20 +0200

    made API 80% faster

--- function_app.py
+++ function_app.py
@@ -3,17 +3,20 @@ import os
 import datetime
 from azure.data.tables import TableClient
 
+connection_string = os.getenv("CosmosDbConnectionSetting")
+
+# Static variables outside of HttpTrigger - improved speed by about 80%
+table_name = "visitorCount"
+table_client = TableClient.from_connection_string(conn_str=connection_string, table_name=table_name)
+entities = list(table_client.list_entities())  # Convert to list
+
 app = func.FunctionApp()
 
 @app.route(route="http_trigger_visitor_counter", auth_level=func.AuthLevel.ANONYMOUS)
 
 def http_trigger_visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
-    connection_string = os.getenv("CosmosDbConnectionSetting")
 
     # Static variables
-    table_name = "visitorCount"
-    table_client = TableClient.from_connection_string(conn_str=connection_string, table_name=table_name)
-    entities = list(table_client.list_entities())  # Convert to list
     date = str(datetime.datetime.now())
 
     # Get client IP
@@ -21,10 +24,13 @@ def http_trigger_visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
     if not visitor_ip:
         visitor_ip = req.headers.get('X-Original-For') or req.headers.get('REMOTE_ADDR')
         if not visitor_ip:
-            visitor_ip = 'Unknown IP'
-    
+            visitor_ip = "Unknown IP"
+
     if ':' in visitor_ip:
         visitor_ip = visitor_ip.split(':')[0] # grab 0 for ip-address - 1 for port-nr
+
+    # Empty list for UnboundLocalError
+    ip_addresses = []
 
     # Get the maximum visitor count and IP addresses
     if entities:
@@ -32,21 +38,13 @@ def http_trigger_visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
         ip_addresses = [entity["RowKey"] for entity in entities]
     else:
         max_visitor_count = 0
-        ip_addresses = []
-
-    # Send max visitor count back to client first
-    response = func.HttpResponse(str(max_visitor_count), status_code=200)
-
-    # Handle update/insert after
-    if not entities:
-        first_visitor = 1
         table_client.create_entity(entity={
             'PartitionKey': date,
             'RowKey': visitor_ip,
-            'VisitorCount': first_visitor
+            'VisitorCount': max_visitor_count
         })
 
-    elif visitor_ip in ip_addresses:
+    if visitor_ip in ip_addresses:
         # Existing IP, no increment - store current max
         table_client.create_entity(entity={
             'PartitionKey': date,
@@ -56,11 +54,11 @@ def http_trigger_visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
         
     else:
         # New visitor, increment the count + 1
-        increment_visitor_count = max_visitor_count + 1
+        max_visitor_count += 1
         table_client.create_entity(entity={
             'PartitionKey': date,
             'RowKey': visitor_ip,
-            'VisitorCount': increment_visitor_count
+            'VisitorCount': max_visitor_count
         })
 
-    return response
+    return func.HttpResponse(str(max_visitor_count), status_code=200)
