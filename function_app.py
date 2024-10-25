c8b6e09abebda5762100433787b362315c03451a
dennisvandijk-Git <dennisvandijk91@live.nl>
2024-10-25 07:26:29 +0200

    fine-tuned API

--- function_app.py
+++ function_app.py
@@ -1,23 +1,27 @@
 import azure.functions as func
 import os
-import datetime
-from azure.data.tables import TableClient
+import json
+from azure.data.tables import TableClient, UpdateMode
 
 connection_string = os.getenv("CosmosDbConnectionSetting")
 
 # Static variables outside of HttpTrigger - improved speed by about 80%
-table_name = "visitorCount"
+table_name = "Visitor_Counter"
 table_client = TableClient.from_connection_string(conn_str=connection_string, table_name=table_name)
-entities = list(table_client.list_entities())  # Convert to list
+
+# Static partition key
+partition_key = "Visitors"
 
 app = func.FunctionApp()
 
-@app.route(route="http_trigger_visitor_counter", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="visitor_counter", auth_level=func.AuthLevel.ANONYMOUS)
 
-def http_trigger_visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
+def visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
 
     # Static variables
-    date = str(datetime.datetime.now())
+    entities = {entity['RowKey']: entity for entity in table_client.list_entities()}
+    # Get max_visitor_count
+    max_visitor_count = max((int(entity["VisitorCount"]) for entity in entities.values()),default = 0)
 
     # Get client IP
     visitor_ip = req.headers.get('X-Forwarded-For')
@@ -28,37 +32,33 @@ def http_trigger_visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
 
     if ':' in visitor_ip:
         visitor_ip = visitor_ip.split(':')[0] # grab 0 for ip-address - 1 for port-nr
+         
+    if visitor_ip in entities:
+        max_visitor_count_same = entities[visitor_ip]['VisitorCount']
 
-    # Empty list for UnboundLocalError
-    ip_addresses = []
-
-    # Get the maximum visitor count and IP addresses
-    if entities:
-        max_visitor_count = max(int(entity["VisitorCount"]) for entity in entities)
-        ip_addresses = [entity["RowKey"] for entity in entities]
+        # Retrieve the existing entity
+        existing_entity = {
+            'PartitionKey': partition_key,
+            'RowKey': entities[visitor_ip]['RowKey'],
+            'VisitorCount': max_visitor_count_same
+        }
+        print(f"Updated entity: {existing_entity}")
+        # Upsert the updated entity
+        table_client.upsert_entity(entity=existing_entity, mode=UpdateMode.MERGE)
     else:
-        max_visitor_count = 0
-        table_client.create_entity(entity={
-            'PartitionKey': date,
-            'RowKey': visitor_ip,
-            'VisitorCount': max_visitor_count
-        })
-
-    if visitor_ip in ip_addresses:
-        # Existing IP, no increment - store current max
-        table_client.create_entity(entity={
-            'PartitionKey': date,
-            'RowKey': visitor_ip,
-            'VisitorCount': max_visitor_count
-        })
-        
-    else:
-        # New visitor, increment the count + 1
+        # Increment visitor_count
         max_visitor_count += 1
-        table_client.create_entity(entity={
-            'PartitionKey': date,
+
+        # Create a new entity for a new visitor
+        new_entity = {
+            'PartitionKey': partition_key,
             'RowKey': visitor_ip,
             'VisitorCount': max_visitor_count
-        })
+        }
+        print(f"Created entity: {new_entity}")
+        # Insert the new entity
+        table_client.upsert_entity(entity=new_entity, mode=UpdateMode.MERGE)
 
-    return func.HttpResponse(str(max_visitor_count), status_code=200)
+    json_response = json.dumps({"VisitorCount": max_visitor_count})
+
+    return func.HttpResponse(json_response, headers={"Content-Type": "application/json"}, status_code=200)
